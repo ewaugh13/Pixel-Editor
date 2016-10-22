b2733509a3b6ab20f4c9ef069fb8b10bf7f00a94
Ryan Steele <ryan.steele.j@gmail.com>
2016-10-22 16:49:24 -0600

    started work on tool.h

--- Gixel/tool.h
+++ Gixel/tool.h
@@ -1,11 +1,16 @@
 #ifndef TOOL_H
 #define TOOL_H
+#include "QPainter"
 
 
-class Tool
+class Tool : public QPainter
 {
 public:
     Tool();
+    QPen line;
+    QBrush fill;
+    QColor color;
+
 };
 
 #endif // TOOL_H
