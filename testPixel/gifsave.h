d94e9ef46950ec20743ca2ce83351bf3458a45b9
Ryan Steele <ryan.steele.j@gmail.com>
2016-11-06 21:17:51 -0700

    Added comments to gifsave

--- testPixel/gifsave.h
+++ testPixel/gifsave.h
@@ -1,3 +1,28 @@
+// Made from gif.h
+// gif.h
+// by Charlie Tangora
+// Public domain.
+// Email me : ctangora -at- gmail -dot- com
+//
+// This file offers a simple, very limited way to create animated GIFs directly in code.
+//
+// Those looking for particular cleverness are likely to be disappointed; it's pretty
+// much a straight-ahead implementation of the GIF format with optional Floyd-Steinberg
+// dithering. (It does at least use delta encoding - only the changed portions of each
+// frame are saved.)
+//
+// So resulting files are often quite large. The hope is that it will be handy nonetheless
+// as a quick and easily-integrated way for programs to spit out animations.
+//
+// Only RGBA8 is currently supported as an input format. (The alpha is ignored.)
+//
+// USAGE:
+// Create a GifWriter struct. Pass it to GifBegin() to initialize and write the header.
+// Pass subsequent frames to GifWriteFrame().
+// Finally, call GifEnd() to close the file handle and free memory.
+//
+
+
 #ifndef GIFSAVE_H
 #define GIFSAVE_H
 
